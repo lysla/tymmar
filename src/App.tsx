d2ab2f68b81eec25510e9a75b6fb38996410982a
Carmen Lysla Ghirardi <carmenghirardi@gmail.com>
2025-10-06 16:11:00 +0200

    maybe need to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
+import { useEffect, useState } from "react";
 import { askAI } from "./api";
 import "./App.css";
 
-async function App() {
-    const text = await askAI("Hello world");
+export default function App() {
+    const [text, setText] = useState<string>("Loading...");
+
+    useEffect(() => {
+        async function fetchAI() {
+            try {
+                const result = await askAI("Hello world");
+                setText(result);
+            } catch (err) {
+                setText("Error contacting AI 😢");
+                console.error(err);
+            }
+        }
+        fetchAI();
+    }, []);
 
     return (
-        <>
-            <div>
-                tymmar updating, understanding checks... even more.
-                <br />
-                {text}
-            </div>
-        </>
+        <div>
+            tymmar updating, understanding checks... even more.
+            <br />
+            {text}
+        </div>
     );
 }
-
-export default App;
