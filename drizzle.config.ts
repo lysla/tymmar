900086c9d226f8cb67e105a767a2c12047d52f1d
Carmen Lysla Ghirardi <carmenghirardi@gmail.com>
2025-10-07 15:11:04 +0200

    fix ref

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
     out: "./drizzle",
-    schema: "./api/db/schema.js",
+    schema: "./api/db/schema.ts",
     dialect: "postgresql",
     dbCredentials: {
         url: process.env.DATABASE_URL!,
